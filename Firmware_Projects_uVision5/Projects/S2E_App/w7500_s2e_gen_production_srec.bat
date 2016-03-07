34067cc1ec6c9b202e1fb300e89b5597446f043f
Eric Jung <myst5829@gmail.com>
2016-03-07 15:12:48 +0900

    Clear

--- Firmware_Projects_uVision5/Projects/S2E_App/w7500_s2e_gen_production_srec.bat
+++
@@ -1,4 +0,0 @@
-copy /b /y obj\W7500x_S2E_App.bin bin\
-copy /b /y obj\W7500x_S2E_App.hex bin\
-..\sRecord\srec_cat.exe ..\S2E_Boot\bin\W7500x_S2E_Boot.hex -intel bin\W7500x_S2E_App.hex -intel -o ..\W7500x_S2E_Production.hex -intel
-..\sRecord\srec_cat.exe ..\W7500x_S2E_Production.hex -intel -o ..\W7500x_S2E_Production.bin -binary
--- Firmware_Projects_uVision5/Projects/S2E_Boot/w7500_s2e_movebin.bat
+++
@@ -1,2 +0,0 @@
-copy /b /y obj\W7500x_S2E_Boot.bin bin\
-copy /b /y obj\W7500x_S2E_Boot.hex bin\
